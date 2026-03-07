718e7b9cf171687453393af4365f65ed17ddf12f
Swan Htet Aung <137768986+swanhtetaung01@users.noreply.github.com>
2026-03-07 14:50:29 +0700

    Initial Commit

---
+++ src/main/java/org/datastructures/linkedlist/Main.java
@@ -0,0 +1,13 @@
+package org.datastructures.linkedlist;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList myLinkedList = new LinkedList(4);
+
+        myLinkedList.getHead();
+        myLinkedList.getTail();
+        myLinkedList.getLength();
+
+        myLinkedList.printList();
+    }
+}
