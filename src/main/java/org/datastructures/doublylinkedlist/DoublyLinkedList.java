4465752069dc74c10ea92740d51a217a3876fc9b
Swan Htet Aung <137768986+swanhtetaung01@users.noreply.github.com>
2026-03-16 23:18:50 +0700

    Add append method to doublylinkedlist

--- src/main/java/org/datastructures/doublylinkedlist/DoublyLinkedList.java
+++ src/main/java/org/datastructures/doublylinkedlist/DoublyLinkedList.java
@@ -21,4 +21,37 @@ public class DoublyLinkedList {
         tail = newNode;
         length = 1;
     }
+
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if(length == 0) {
+            head = newNode;
+        }else {
+        tail.next = newNode;
+        newNode.prev = tail;
+        }
+        tail = newNode;
+        length++;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while(temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void getHead() {
+        System.out.println("Head: " + head.value);
+    }
+
+    public void getTail() {
+        System.out.println("Tail: " + tail.value);
+    }
+
+    public void getLength() {
+        System.out.println("Length: " + length);
+    }
+
 }
