8ecfa3f84606a3a6c620a340974f4088c6ce557f
Swan Htet Aung <137768986+swanhtetaung01@users.noreply.github.com>
2026-03-16 23:41:52 +0700

    Add removeFirst method to doublylinkedlist

--- src/main/java/org/datastructures/doublylinkedlist/DoublyLinkedList.java
+++ src/main/java/org/datastructures/doublylinkedlist/DoublyLinkedList.java
@@ -62,6 +62,21 @@ public class DoublyLinkedList {
         length++;
     }
 
+    public Node removeFirst() {
+        if(length == 0) return null;
+        Node temp = head;
+        if(length == 1) {
+            head = null;
+            tail = null;
+        }else {
+            head = head.next;
+            head.prev = null;
+            temp.next = null;
+        }
+        length--;
+        return temp;
+    }
+
     public void printList() {
         Node temp = head;
         while(temp != null) {
