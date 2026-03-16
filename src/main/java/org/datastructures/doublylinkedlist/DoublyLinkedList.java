7f97c1af93571cf36ce3fa472a435bd76ef49bfc
Swan Htet Aung <137768986+swanhtetaung01@users.noreply.github.com>
2026-03-16 23:25:59 +0700

    Add removeLast method to doublylinkedlist

--- src/main/java/org/datastructures/doublylinkedlist/DoublyLinkedList.java
+++ src/main/java/org/datastructures/doublylinkedlist/DoublyLinkedList.java
@@ -34,6 +34,21 @@ public class DoublyLinkedList {
         length++;
     }
 
+    public Node removeLast() {
+        if(length == 0) return null;
+        Node temp = tail;
+        if(length == 1) {
+            head = null;
+            tail = null;
+        }else {
+            tail = tail.prev;
+            tail.next = null;
+            temp.prev = null;
+        }
+        length--;
+        return temp;
+    }
+
     public void printList() {
         Node temp = head;
         while(temp != null) {
---
+++ src/main/java/org/datastructures/doublylinkedlist/Main.java
@@ -0,0 +1,7 @@
+package org.datastructures.doublylinkedlist;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
