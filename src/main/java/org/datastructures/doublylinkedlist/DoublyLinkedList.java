c03200eb9512af9e0b0160f385d6af1864ac93c0
Swan Htet Aung <137768986+swanhtetaung01@users.noreply.github.com>
2026-03-17 23:51:50 +0700

    Add insert and remove methods in doublylinkedlist

--- src/main/java/org/datastructures/doublylinkedlist/DoublyLinkedList.java
+++ src/main/java/org/datastructures/doublylinkedlist/DoublyLinkedList.java
@@ -102,6 +102,40 @@ public class DoublyLinkedList {
         return false;
     }
 
+    public boolean insert(int index, int value) {
+        if(index < 0 || index > length) return false;
+        if(index == 0) {
+             prepend(value);
+             return true;
+        }
+        if(index == length) {
+            append(value);
+            return true;
+        }
+        Node newNode = new Node(value);
+        Node before = get(index - 1);
+        Node after = before.next;
+        before.next = newNode;
+        newNode.prev = before;
+        newNode.next = after;
+        after.prev = newNode;
+        length++;
+        return true;
+    }
+
+    public Node remove(int index) {
+        if(index < 0 || index > length) return null;
+        if(index == 0) return removeFirst();
+        if(index == length - 1) return removeLast();
+        Node temp = get(index);
+        temp.next.prev = temp.prev;
+        temp.prev.next = temp.next;
+        temp.prev = null;
+        temp.next = null;
+        length--;
+        return temp;
+    }
+
     public void printList() {
         Node temp = head;
         while(temp != null) {
